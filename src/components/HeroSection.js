f460d71f826c9a6d6615399a4280b07555d6646e
Stef Verlinde <stef.verlinde@student.hogent.be>
2020-10-21 13:19:50 +0200

    Changing background

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,7 +5,7 @@ import { Link } from "react-scroll";
 function HeroSection() {
   return (
     <div className="hero-c">
-      <video src="/videos/video-4.mp4" autoPlay loop muted />
+      {/* <video src="/videos/video-4.mp4" autoPlay loop muted /> */}
       <img src="/images/StefVerlinde.png" alt="profile pic" className="rounded-circle profilep"/>
       <h1>Stef Verlinde</h1>
       <p>Full-stack developer</p>
